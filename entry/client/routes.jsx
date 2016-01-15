4b0b14e6c09e7ace8385fa70f4504d213278d2cc
Bill Fienberg <wfienberg2@hotmail.com>
2016-01-14 20:42:30 -0600

    Finish step 11

---
+++ entry/client/routes.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router, Route, Link, IndexRoute } from "react-router";
+import createBrowserHistory from 'history/lib/createBrowserHistory';
+
+import Home from "Home/routes";
+import Settings from "Settings/routes";
+import Other from "Other/routes";
+
+const App = react.createClass({
+  render() {
+    return <div>{this.props.children}</div>
+  }
+})
+
+const rootRoute = {
+  component: "div",
+  childRoutes: [{
+    path: "/",
+    component: App,
+    indexRoute: Home,
+    childRoutes: [
+      Settings, 
+      Other
+    ]
+  }]
+}
+
+ReactDOM.render(
+  <Router history={createBrowserHistory()} routes={rootRoute} />,
+  document.getElementById("app")
+)
